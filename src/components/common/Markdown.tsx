d12ebd55540e383c0b127255903be647daf323d2
Chiko <2452559902@qq.com>
2024-01-06 15:53:21 +0800

    opt: 优化markdown组价

--- src/components/common/Markdown.tsx
+++ src/components/common/Markdown.tsx
@@ -1,10 +1,33 @@
 import { ReactNode, memo } from "react";
 import ReactMarkdown, { Options } from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { Praise as SyntaxHighlighter } from "react-syntax-highlighter";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function Markdown({ children, className = "", ...props }: Options) {
-  return <ReactMarkdown components={{}}>{children}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      components={{
+        code({ node, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return match ? (
+            <SyntaxHighlighter
+              PreTag="div"
+              children={String(children).replace(/\n$/, "")}
+              language={match[1]}
+              style={a11yDark}
+            />
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  );
 }
 
 export default memo(Markdown); // 缓存组件
